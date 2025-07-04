47d6a3edd3a1af882eb472a22c950b709652e113
zTrix <i@ztrix.me>
2025-07-04 23:56:00 +0800

    license for qemu code

---
+++ qemu_compose/qemu/__init__.py
@@ -0,0 +1,3 @@
+
+# code under this package comes from https://gitlab.com/qemu-project/qemu/-/tree/master/python/qemu with small modification
+# they are licensed under https://gitlab.com/qemu-project/qemu/-/blob/master/LICENSE
