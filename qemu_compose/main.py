9242dbabe2efdd533b98ae71e06a5ebc2e5ee435
zTrix <i@ztrix.me>
2025-10-06 16:41:25 +0800

    bump version to v0.8.2

--- qemu_compose/main.py
+++ qemu_compose/main.py
@@ -61,7 +61,7 @@ def guess_conf_path(p:str | None):
     return None
 
 def version(short=False):
-    version = "v0.8.0"
+    version = "v0.8.2"
     if short:
         print(version, file=sys.stderr)
     else:
