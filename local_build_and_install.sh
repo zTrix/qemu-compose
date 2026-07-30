55b31ce3247c7e15bf957904befed380a96e5740
zTrix <i@ztrix.me>
2026-07-30 23:20:46 +0800

    update local_build_and_install.sh

--- local_build_and_install.sh
+++ local_build_and_install.sh
@@ -3,4 +3,4 @@
 set -ex
 rm -rf ./dist
 uv build
-sudo pip install --force-reinstall --break-system-packages --no-deps ./dist/qemu_compose-*.whl
+sudo pip3 install --force-reinstall --break-system-packages --no-deps --upgrade ./dist/qemu_compose-*.whl
